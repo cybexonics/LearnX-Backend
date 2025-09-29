c7e546e6fcc7f22446129f30c8a0414e1a43b2d4
Bhavya-Ayasya <bhavya.chawla@ayasya.com>
2025-09-29 19:14:09 +0530

    changes in a backend code

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(cookieParser());
 // CORS configuration - more secure
 app.use(
   cors({
-    origin:["https://learn-x-website-nirv.vercel.app"], // Use environment variable for client URL
+    origin:["https://learn-x-website-nirv-m2h0r6kj9.vercel.app", "https://learnx-backend-h6h0.onrender.com"], // Use environment variable for client URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
